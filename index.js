a85232dc1bd001ef74cd868626d308bc99a6e7fe
ArtsiomChults <artemorsha@mail.ru>
2025-04-22 15:28:58 +0300

    fix: cron message every minute --2

--- index.js
+++ index.js
@@ -157,17 +157,19 @@ const startBot = async () => {
 			return bot.sendMessage(chatId, `К сожалению ты не угадал, бот загадал цифру ${chats[chatId]}`, againGameOptions)
 		}
 	})
+
+
+	//subscribe message
+
+	cron.schedule('0 */2 * * *', () => {
+		console.log('Текущее время:', new Date());
+		subscribers.forEach(chatId => {
+			bot.sendMessage(chatId, '👋 Доброе утро! Это твоя автоматическая рассылка.');
+		});
+	});
+
 };
 
-
-//subscribe message
-
-cron.schedule('* * * * *', () => {
-	console.log('Текущее время:', new Date());
-	subscribers.forEach(chatId => {
-		bot.sendMessage(chatId, '👋 Доброе утро! Это твоя автоматическая рассылка.');
-	});
-});
 
 startBot();
 
